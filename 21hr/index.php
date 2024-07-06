88b6f92beb50340ffa5a27ae81f3847f28cf859b
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2024-07-06 11:05:40 +0530

    Add files via upload

---
+++ 21hr/index.php
@@ -0,0 +1,66 @@
+<?php
+// Variables
+// $first="Aman";
+// $First=" Kumar";
+//  echo $first.$First." Hello!"." 1"."Yeah";//concatenation
+
+//  Operators
+
+// Arithmetic Operator
+    // $a=10;
+    // $b=3;
+    // $c=$a+$b;
+    // echo "Add =\t".$c;
+    // $c=$a-$b;
+    // echo "\nSub =\t".$c;
+    // $c=$a*$b;
+    // echo "\nMul =\t".$c;
+    // $c=$a/$b;
+    // echo "\nDiv =\t".$c;
+    // $c=$a%$b;
+    // echo "\nMod =\t".$c;
+    // $c++;
+    // echo "\nInc =\t".$c;
+    // $c--;
+    // echo "\nDec =\t".$c;
+// Comparision Operator
+    //    $x=3;
+    //    $y=3;
+    //    var_dump($x==$y);
+    //    var_dump($x!=$y);
+    //    var_dump($x<$y);
+    //    var_dump($x>$y);
+    //    var_dump($x<=$y);
+    //    var_dump($x>=$y);
+// Logical and conditional operator 
+    //  $a=1;//true
+    //  $b=0;//false
+
+    //  $c= $a && $b;//AND
+    //  var_dump($c);
+
+    //  $c= $a || $b;//OR
+    //  var_dump($c);
+
+    //  $d=!$a;
+    //  var_dump($d);
+
+    //Ternary Operator
+    // $x=5;
+    // $x > 3 ? $flag ="YES" : $flag= "NO";
+    // var_dump($flag) ;
+
+//Assignment Operator
+    $a=5;
+    $b=$a; //Simple Assignment
+    echo "\nSimple Assignment";var_dump($b);
+    $b+=$a; //Addition Assignment
+    echo "\nAddition Assignment"; var_dump($b);
+    $b-=$a; //Subtraction Assignment
+    echo "\nSubtraction Assignment"; var_dump($b);
+    $b*=$a; //Multiplication Assignment
+    echo "\nMultiplication Assignment"; var_dump($b);
+    $b/=$a; //Division Assignment
+    echo "\nDivision Assignment"; var_dump($b);
+    
+?>
